37d9c5482181e5a92c658b082b27c6b2d77c5881
AsBergs <anasofiabergsoares06@gmail.com>
2024-07-23 16:34:39 -0300

    nsei

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.uf2ibb55mr"
+    "revision": "0.e0ioaignj4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
