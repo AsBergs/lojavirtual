25c7946c6af4fed540b4cc39a247b79bb2e0ecbb
AsBergs <anasofiabergsoares06@gmail.com>
2024-07-26 08:08:11 -0300

    pagina view e apis

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import LayoutLarge from '@/layouts/LayoutLarge.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import CategoriaView from "../views/CategoriaView.vue";
+import AcessorioView from "../views/AcessorioView.vue";
+import CorView from "../views/CorView.vue";
+import MarcaView from "../views/MarcaView.vue";
+import ModeloView from "../views/ModeloView.vue";
+import VeiculoView from "../views/VeiculoView.vue"
+import LoginView from "../views/LoginView.vue";
+import LogoutView from "../views/LogoutView.vue";
+import LayoutFull from '@/layouts/LayoutFull.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,26 +16,55 @@ const router = createRouter({
     {
       path: '/',
       name: '',
-      component: LayoutLarge,
+      component: LayoutFull,
       children: [
         {
-          path: '',
-          name: 'Home',
-          component: HomeView
+          path: "",
+          name: "home",
+          component: HomeView,
         },
         {
-          path: '/login',
-          name: 'Login',
-          component: () => import('@/views/LoginView.vue')
+          path: "/categorias",
+          name: "categorias",
+          component: CategoriaView,
         },
         {
-          path: '/produtos/adicionar',
-          name: 'ProductAdd',
-          component: () => import('@/views/ProductAdd.vue')
-        }
-      ]
+          path: "/acessorios",
+          name: "acessorios",
+          component: AcessorioView,
+        },
+        {
+          path: "/cores",
+          name: "cores",
+          component: CorView,
+        },
+        {
+          path: "/marcas",
+          name: "marcas",
+          component: MarcaView,
+        },
+        {
+          path: "/modelos",
+          name: "modelos",
+          component: ModeloView,
+        },
+        {
+          path: "/veiculos",
+          name: "veiculos",
+          component: VeiculoView,
+        },
+        {
+          path: "/login",
+          name: "login",
+          component: LoginView,
+        },
+        {
+          path: "/logout",
+          name: "logout",
+          component: LogoutView,
+        }      ]
     }
   ]
-})
+});
 
-export default router
+export default router;
---
+++ src/views/VeiculoView.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { ref, reactive, onMounted } from 'vue'
+import VeiculosApi from '@/api/veiculos'
+import CoresApi from '@/api/cores'
+import ModelosApi from '@/api/modelos'
+import AcessoriosApi from '@/api/acessorios'
+import Button from '../components/ButtonComp.vue'
+import NumberField from '../components/NumberFieldComponent.vue'
+import Line from '@/components/LineComponent.vue'
+import Select from '../components/SelectComponent.vue'
+
+const veiculosApi = new VeiculosApi()
+const coresApi = new CoresApi()
+const modelosApi = new ModelosApi()
+const acessoriosApi = new AcessoriosApi()
+const defaultVeiculos = { id: null, ano: '', preco: '', acessorio: '', modelo: '', cor: '' }
+const veiculos = ref([])
+const cores = ref([])
+const modelos = ref([])
+const acessorios = ref([])
+const veiculo = reactive({ ...defaultVeiculos })
+
+onMounted(async () => {
+  veiculos.value = await veiculosApi.buscarTodosOsVeiculos()
+  cores.value = await coresApi.buscarTodasAsCores()
+  modelos.value = await modelosApi.buscarTodosOsModelos()
+  acessorios.value = await acessoriosApi.buscarTodosOsAcessorios()
+})
+
+function limpar() {
+  Object.assign(veiculo, { ...defaultVeiculos })
+}
+async function salvar() {
+  if (veiculo.id) {
+    await veiculosApi.atualizarVeiculo(veiculo)
+  } else {
+    await veiculosApi.adicionarVeiculo(veiculo)
+  }
+  veiculos.value = await veiculosApi.buscarTodosOsVeiculos()
+  limpar()
+}
+
+
+</script>
+
+<template>
+  <div class="main">
+    <h1>Veículos</h1>
+    <div class="container-select">
+      <div class="form">
+        <NumberField v-model="veiculo.ano" label="ano"  />
+        <NumberField v-model="veiculo.preco" label="preço"  />
+        <Select v-model="veiculo.cor" label="cores" :list="cores" />
+        <Select v-model="veiculo.modelo" label="modelos" :list="modelos" />
+        <Select v-model="veiculo.acessorio" label="acessorios" :list="acessorios" />
+        <Button @salvar="salvar()" @limpar="limpar()" />
+      </div>
+      <Line :veiculos="veiculos" />
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
